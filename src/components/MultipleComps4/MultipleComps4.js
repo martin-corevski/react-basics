1b467c8c4bba969648511417e24644dff630e575
Martin Corevski <martin-corevski@outlook.com>
2018-04-17 22:38:09 +0200

    Animations and transitions updates
    react-transition-group added
    List animations with Transition group and CSS transtion components

--- src/components/MultipleComps4/MultipleComps4.js
+++ src/components/MultipleComps4/MultipleComps4.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import TransitionGroup from 'react-transition-group/TransitionGroup'
+import CSSTransition from 'react-transition-group/CSSTransition'
+
+import classes from './MultipleComps4.scss'
 import Comp4 from '../Comp4/Comp4'
 
-const multipleComps4 = props => props.components.map((comp, index) => {
-  return <Comp4
-    key={comp.id}
-    name={comp.name}
-    click={props.clicked.bind(this, index)}
-    change={(event) => props.changed(event, comp.id)} />
-})
+class multipleComps4 extends Component {
+  render () {
+    const list = this.props.components.map((comp, index) => {
+      return (
+        <CSSTransition
+          key={index}
+          classNames={{
+            enter: '',
+            enterActive: '',
+            exit: classes.fadeExit,
+            exitActive: classes.fadeExitActive
+          }}
+          timeout={300}
+        >
+          <Comp4
+            key={comp.id}
+            name={comp.name}
+            click={this.props.clicked.bind(this, index)}
+            change={event => this.props.changed(event, comp.id)}
+          />
+        </CSSTransition>
+      )
+    })
+    return <TransitionGroup>{list}</TransitionGroup>
+  }
+}
 
 export default multipleComps4
